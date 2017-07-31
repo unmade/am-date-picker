7002e2dcd2f1eec6e033ce5ba96f07926dce01ed
Galias <pawel@galias.me>
2017-07-31 13:10:41 +0200

    Change styles for am-date-picker icons

--- demo/app.js
+++ demo/app.js
@@ -15,6 +15,7 @@ angular
             var today = new Date();
             $scope.to = new Date(today.setDate(today.getDate() + 1));
             $scope.from = new Date('2015-11-05');
+            $scope.date = today;
             $scope.onChange = function(date) {
                 console.log('date was changed to %s', date);
             };
